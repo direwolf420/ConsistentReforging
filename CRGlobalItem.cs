f0d05fd0b19a3a8ee8d7399d6a9b1df89d15662f
direwolf420 <amakejk1@web.de>
2021-04-04 12:29:37 +0200

    unloaded mod prefix saving/loading

--- CRGlobalItem.cs
+++ CRGlobalItem.cs
@@ -25,6 +25,9 @@ namespace ConsistentReforging
 			0
 		};
 
+		//Orphaned mod prefixes will end up in this list until the mod is enabled again (or the prefix is re-added)
+		public Dictionary<string, List<string>> orphanedModPrefixes = new Dictionary<string, List<string>>();
+
 		private const int InvalidIndex = -1;
 		private const int InvalidPrefix = 0;
 
@@ -52,6 +55,7 @@ namespace ConsistentReforging
 			CRGlobalItem myClone = (CRGlobalItem)base.Clone(item, itemClone);
 
 			myClone.reforges = new List<int>(reforges);
+			myClone.orphanedModPrefixes = new Dictionary<string, List<string>>(orphanedModPrefixes);
 
 			return myClone;
 		}
@@ -72,10 +76,77 @@ namespace ConsistentReforging
 
 		public override TagCompound Save(Item item)
 		{
-			TagCompound tag = new TagCompound()
+			List<int> vanillaReforges = reforges.Where(p => p < PrefixID.Count).ToList();
+
+			List<string> asd = reforges.Where(p => p >= PrefixID.Count).Select(p => ModPrefix.GetPrefix((byte)p).Name).ToList();
+
+			Dictionary<string, List<string>> modReforges = new Dictionary<string, List<string>>();
+
+			foreach (var p in reforges)
 			{
-				{"reforges", reforges }
-			};
+				if (p < PrefixID.Count)
+				{
+					continue;
+				}
+
+				ModPrefix modPrefix = ModPrefix.GetPrefix((byte)p);
+
+				if (modPrefix == null)
+				{
+					continue;
+				}
+
+				Mod mod = modPrefix.mod;
+
+				if (mod == null)
+				{
+					continue;
+				}
+
+				string modName = mod.Name;
+				string name = modPrefix.Name;
+
+				if (!modReforges.ContainsKey(modName))
+				{
+					modReforges[modName] = new List<string>();
+				}
+
+				var reforgeNames = modReforges[modName];
+
+				if (!reforgeNames.Contains(name))
+				{
+					reforgeNames.Add(name);
+				}
+			}
+
+			//Dictionary to TagCompound
+
+			TagCompound tag = new TagCompound();
+
+			TagCompound moddedReforges = new TagCompound();
+
+			foreach (var pair in modReforges)
+			{
+				moddedReforges.Add(pair.Key, pair.Value);
+			}
+
+			TagCompound orphanedModdedReforges = new TagCompound();
+
+			foreach (var pair in orphanedModPrefixes)
+			{
+				orphanedModdedReforges.Add(pair.Key, pair.Value);
+			}
+
+			tag.Add("vanillaReforges", vanillaReforges);
+			tag.Add("moddedReforges", moddedReforges);
+			tag.Add("orphanedModdedReforges", orphanedModdedReforges);
+
+			//Old
+			//TagCompound tag = new TagCompound()
+			//{
+			//	{"reforges", reforges }
+			//};
+
 			return tag;
 		}
 
@@ -83,7 +154,84 @@ namespace ConsistentReforging
 		{
 			if (!Config.Instance.SaveReforges) return;
 
-			reforges = (List<int>)tag.GetList<int>("reforges");
+			//Old compatibility
+			if (tag.ContainsKey("reforges"))
+			{
+				reforges = (List<int>)tag.GetList<int>("reforges");
+				return;
+			}
+
+			List<int> vanillaReforges = (List<int>)tag.GetList<int>("vanillaReforges");
+
+			List<int> modReforges = new List<int>();
+
+			TagCompound moddedReforges = tag.Get<TagCompound>("moddedReforges");
+
+			TagCompound orphanedModdedReforges = tag.Get<TagCompound>("orphanedModdedReforges");
+
+			//Merge previously loaded + orphaned together, and then filter through them at once
+
+			foreach (var orphanedModTag in orphanedModdedReforges)
+			{
+				string modName = orphanedModTag.Key;
+				var orphanNames = orphanedModTag.Value as List<string> ?? new List<string>();
+
+				if (moddedReforges.ContainsKey(modName))
+				{
+					var reforgeNames = moddedReforges[modName] as List<string> ?? new List<string>();
+
+					moddedReforges[modName] = reforgeNames.Union(orphanNames).ToList();
+				}
+				else
+				{
+					moddedReforges[modName] = orphanNames;
+				}
+			}
+
+			orphanedModPrefixes = new Dictionary<string, List<string>>();
+
+			foreach (var modTag in moddedReforges)
+			{
+				string modName = modTag.Key;
+				var reforgeNames = modTag.Value as List<string> ?? new List<string>();
+
+				Mod mod = ModLoader.GetMod(modName);
+
+				if (mod == null)
+				{
+					if (!orphanedModPrefixes.ContainsKey(modName))
+					{
+						//If mod is null (not currently loaded), all its prefixes are also not loaded, so save the entire list, and continue
+						orphanedModPrefixes[modName] = reforgeNames;
+					}
+
+					continue;
+				}
+
+				foreach (var name in reforgeNames)
+				{
+					ModPrefix modPrefix = mod.GetPrefix(name);
+					if (modPrefix == null)
+					{
+						//If loaded prefix does not exist, add it as orphaned
+						if (!orphanedModPrefixes.ContainsKey(modName))
+						{
+							orphanedModPrefixes[modName] = new List<string>();
+						}
+
+						List<string> lists = orphanedModPrefixes[modName];
+						if (!lists.Contains(name))
+						{
+							lists.Add(name);
+						}
+						continue;
+					}
+
+					modReforges.Add(modPrefix.Type);
+				}
+			}
+
+			reforges = vanillaReforges.Union(modReforges).ToList();
 		}
 
 		public override void NetSend(Item item, BinaryWriter writer)
@@ -94,6 +242,23 @@ namespace ConsistentReforging
 			{
 				writer.Write((byte)reforges[i]);
 			}
+
+			//Send orphaned mod count first
+			writer.Write((int)orphanedModPrefixes.Count);
+
+			foreach (var pair in orphanedModPrefixes)
+			{
+				//Mod name
+				writer.Write((string)pair.Key);
+				var list = pair.Value;
+
+				//By extension, a mod can't have more than 255 prefixes
+				writer.Write((byte)list.Count);
+				for (int i = 0; i < list.Count; i++)
+				{
+					writer.Write((string)list[i]);
+				}
+			}
 		}
 
 		public override void NetReceive(Item item, BinaryReader reader)
@@ -103,6 +268,27 @@ namespace ConsistentReforging
 			for (int i = 0; i < count; i++)
 			{
 				reforges.Add(reader.ReadByte());
+			}
+
+			int orphanCount = reader.ReadInt32();
+
+			orphanedModPrefixes = new Dictionary<string, List<string>>();
+
+			for (int i = 0; i < orphanCount; i++)
+			{
+				string modName = reader.ReadString();
+
+				orphanedModPrefixes[modName] = new List<string>();
+
+				byte prefixCount = reader.ReadByte();
+
+				for (int j = 0; j < prefixCount; j++)
+				{
+					string name = reader.ReadString();
+
+					List<string> lists = orphanedModPrefixes[modName];
+					lists.Add(name);
+				}
 			}
 		}
 
